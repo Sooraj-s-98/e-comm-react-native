f84fce32f6591d49e9ade6a68b4a25ef2ed38c28
sooraj <development@liiighthouse.com>
2021-05-21 19:12:59 +0530

    fix

--- back-end/app.js
+++ back-end/app.js
@@ -1,19 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require('dotenv/config');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
 
 app.use(cors());
 app.options('*', cors())
 
 //middleware
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(morgan('tiny'));
-
-const api = process.env.API_URL;
+app.use(authJwt());
+app.use(errorHandler);
 
 //Routes
 const categoriesRoutes = require('./routes/categories');
@@ -21,11 +23,14 @@ const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
 const ordersRoutes = require('./routes/orders');
 
+const api = process.env.API_URL;
+
 app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
+//Database
 mongoose.connect(`${process.env.CONNECT_STRING}`,
     {
         useUnifiedTopology: true,
