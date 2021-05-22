bc1a025318691334c8179d5338abfda77d166cad
sooraj <development@liiighthouse.com>
2021-05-22 10:34:55 +0530

    user router completed

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -22,7 +22,6 @@ router.get('/:id', async (req, res) => {
     res.status(200).send(user);
 })
 
-
 router.post('/', async (req, res) => {
     let user = new User({
         name: req.body.name,
@@ -37,6 +36,39 @@ router.post('/', async (req, res) => {
         country: req.body.country,
     })
     user = await user.save();
+
+    if (!user)
+        return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
+
+router.put('/:id', async (req, res) => {
+
+    const userExist = await User.findById(req.params.id);
+    let newPassword
+    if (req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10)
+    } else {
+        newPassword = userExist.passwordHash;
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: newPassword,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true }
+    )
 
     if (!user)
         return res.status(400).send('the user cannot be created!')
@@ -69,6 +101,51 @@ router.post('/login', async (req, res) => {
 
 })
 
+
+router.post('/register', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if (!user)
+        return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
+
+
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id).then(user => {
+        if (user) {
+            return res.status(200).json({ success: true, message: 'the user is deleted!' })
+        } else {
+            return res.status(404).json({ success: false, message: "user not found!" })
+        }
+    }).catch(err => {
+        return res.status(500).json({ success: false, error: err })
+    })
+})
+
+router.get(`/get/count`, async (req, res) => {
+    const userCount = await User.countDocuments((count) => count)
+
+    if (!userCount) {
+        res.status(500).json({ success: false })
+    }
+    res.send({
+        userCount: userCount
+    });
+})
 
 
 module.exports = router;
